d9970ca677f6d304d74e4b1c8568eeafa16b1047
CeeTortle <christurel741@gmail.com>
2025-01-26 14:00:27 -0800

    more elvator stuff

---
+++ commands/elevatorCommands.py
@@ -0,0 +1,5 @@
+import commands2,wpilib
+from subsystems.elevator import elevatorSubsystem
+
+class AxisControl(commands2.command):
+    pass
--- subsystems/elevator.py
+++ subsystems/elevator.py
@@ -1,7 +1,12 @@
 import rev,wpilib,commands2
 from RobotConfig import elevator
 
-class elevatorSubSystem(commands2.subsystem):
+class elevatorSubsystem(commands2.subsystem):
     def __init__(self):
-        self.lMotorID=elevator.leftMotorID
-        self.rMotorID=elevator.rightMotorID
+        lMotorID=elevator.leftMotorID
+        rMotorID=elevator.rightMotorID
+        self.lMotor=rev.CANSparkMax(lMotorID,rev.CANSparkMax.MotorType.kBrushless)
+        self.rMotor=rev.CANSparkMax(rMotorID,rev.CANSparkMax.MotorType.kBrushless)
+        self.lMotor.follow(self.rMotor,True)
+    def axisControl(self,axisIn):
+        self.rMotor.set(axisIn)
