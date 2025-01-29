09d7960185b37cdcc1a245f8dabecb964cb77e0a
CeeTortle <christurel741@gmail.com>
2025-01-28 17:37:06 -0800

    Update elevatorCommand.py

--- commands/elevatorCommand.py
+++ commands/elevatorCommand.py
@@ -1,5 +1,5 @@
 import commands2,wpilib
-from subsystems.elevator import elevatorSubSystem
+from subsystems.elevator import elevatorSubSystem,posElevatorSubsystem
 class elevatorUp(commands2.Command,):
     def __init__(self, kElevSub:elevatorSubSystem):
         super().__init__()
@@ -20,5 +20,13 @@ class elevatorDown(commands2.Command,):
         return super().execute()
     def end(self, interrupted:bool):
         self.eSub.idle
-
+class elevatorToPos(commands2.Command,):
+    def __init__(self, kElevSub:posElevatorSubsystem):
+        super().__init__()
+        self.addRequirements
+        self.eSub=kElevSub
+        self.xbox=wpilib.XboxController(1)
+    def execute(self):
+        self.eSub.setPosition(self.xbox.getLeftY())
+        return super().execute()
 
