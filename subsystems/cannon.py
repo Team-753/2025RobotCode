c1cf28b60c6bd71a580b1e725dda5257f5db00c3
CeeTortle <christurel741@gmail.com>
2025-01-29 17:23:29 -0800

    Update cannon.py

--- subsystems/cannon.py
+++ subsystems/cannon.py
@@ -4,22 +4,31 @@ import RobotConfig
 
 class CannonSubsystem(commands2.Subsystem):
     
-    def __init__(self) -> None:
+    def __init__(self):
         self.TopMotor = rev.CANSparkMax(RobotConfig.coralCannon.TopMotorID, rev.CANSparkMax.MotorType.kBrushed)
         self.BottomMotor = rev.CANSparkMax(RobotConfig.coralCannon.BottomMotorID, rev.CANSparkMax.MotorType.kBrushed)
         self.BottomMotor.setInverted(True)
+        self.pivotMotor = rev.CANSparkMax(RobotConfig.coralCannon.pivotMotorID, rev.CANSparkMax.MotorType.kBrushless)
 
-        
-    def place(self) -> None:
+    def place(self):
         print(self.TopMotor)
         self.TopMotor.set(1)
         self.BottomMotor.set(1)
         
-    def intake(self) -> None:
+    def intake(self):
         print(self.TopMotor)
         self.TopMotor.set(-1)
         self.BottomMotor.set(-1)
 
-    def idle(self) -> None:
+    def idle(self):
         self.TopMotor.set(0)
         self.BottomMotor.set(0)
+
+    def pivotup(self):
+        print(self.pivotMotor)
+        self.pivotMotor.set(1)
+
+    def pivotdown(self):
+        print(self.pivotMotor)
+        self.pivotMotor.set(-1)
+
