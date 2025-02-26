67d0dea7f859917233f28c430affe491a3312e44
RyanTrissel <ryankmarks@gmail.com>
2025-02-25 21:47:04 -0800

    AI

--- robotContainer.py
+++ robotContainer.py
@@ -2,144 +2,106 @@ from subsystems.drivetrain import DriveTrainSubSystem
 from commands.defaultDriveCommand import DefaultDriveCommand, SlowDown
 import commands2
 import wpilib
-import os
 from wpimath import geometry, kinematics, estimator
 import RobotConfig as config
-import commands2
 import commands2.button
 import commands2.cmd
 from commands2.sysid import SysIdRoutine
 
-
-from commands.cannonCommand import place, intake, DefaultPivotCommand,cannonToPosition, PivotDown, PivotUp
-from commands.AlgaeCommand import GrabAlgae,ReleaseAlgae, FlipAlgaeSquisher
-from commands.elevatorCommand import elevatorUp,elevatorDown,elevatorToPos
+from commands.cannonCommand import place, intake, DefaultPivotCommand, cannonToPosition, PivotDown, PivotUp
+from commands.AlgaeCommand import GrabAlgae, ReleaseAlgae, FlipAlgaeSquisher
+from commands.elevatorCommand import elevatorUp, elevatorDown, elevatorToPos
 from commands.ClimberCommand import FlipClimber, FlipCompressor
 
 from commands.VisionCommand import Lock
-
 from commands.simpleAutoCommands import *
 
 from subsystems.limelight_camera import LimelightCamera
-
 from subsystems.cannon import CannonSubsystem
 from subsystems.algae import AlgaeSquisher
 from subsystems.elevator import elevatorSubSystem
 from subsystems.Climber import ClimberSubsystem
 
-#from pathplannerlib.auto import AutoBuilder
-#from phoenix6 import swerve, hardware
 from wpilib import SmartDashboard
 from wpimath.geometry import Rotation2d
-#from wpimath.units import rotationsToRadians
 
 class RobotContainer():
     _max_speed = 1
     _max_angular_rate = 1
     _BLUE_ALLIANCE_PERSPECTIVE_ROTATION = Rotation2d.fromDegrees(0)
     _RED_ALLIANCE_PERSPECTIVE_ROTATION = Rotation2d.fromDegrees(180)
+    
     def __init__(self) -> None:
-        #declaring the subsystems and setting up the drivetrain control
-
-
+        # Declaring subsystems and setting up drivetrain control.
         self.limelight = LimelightCamera("limelight-jamal")
-
-
         self.joystick = commands2.button.CommandJoystick(0)
         self.AuxController = commands2.button.CommandXboxController(1)
-
         self.driveTrain = DriveTrainSubSystem(self.joystick)
         self.elevator = elevatorSubSystem()
-        
         self.driveTrain.setDefaultCommand(DefaultDriveCommand(self.driveTrain))
-        
         self.scheduler = commands2.CommandScheduler()
-
         self.algae = AlgaeSquisher()
         self.cannon = CannonSubsystem()
         self.climber = ClimberSubsystem()
         
-        
-        #sets the climber down off rip
+        # Set the climber down immediately.
         self.climber.GoDown()
         self.climber.ComeBack()
 
-        # Path follower
+        # Path follower auto-chooser.
         self._auto_chooser = wpilib.SendableChooser()
         self._auto_chooser.setDefaultOption("forward", superSimpleAuto(self.driveTrain, [0, 1, 0], 2))
         SmartDashboard.putData("Auto Mode", self._auto_chooser)
 
-        # Configure the button bindings
+        # Configure button bindings.
         self.configureButtonBindings()
 
-
     def configureButtonBindings(self) -> None:
-        self.AuxController.rightTrigger(0.5).whileTrue(place(self.cannon)) 
+        self.AuxController.rightTrigger(0.5).whileTrue(place(self.cannon))
         self.AuxController.leftTrigger(0.5).whileTrue(intake(self.cannon))
-        
-        
         self.AuxController.rightBumper().whileTrue(FlipClimber(self.climber))
         self.AuxController.leftBumper().whileTrue(FlipAlgaeSquisher(self.climber))
+        self.AuxController.a().onTrue(elevatorToPos(self.elevator, 6))
+        self.AuxController.b().onTrue(elevatorToPos(self.elevator, 13))
+        self.AuxController.y().onTrue(elevatorToPos(self.elevator, 22))
+        self.AuxController.x().onTrue(elevatorToPos(self.elevator, 0))
         
-        
-        self.AuxController.a().onTrue(elevatorToPos(self.elevator,6))
-        self.AuxController.b().onTrue(elevatorToPos(self.elevator,13))
-        self.AuxController.y().onTrue(elevatorToPos(self.elevator,22))
-        self.AuxController.x().onTrue(elevatorToPos(self.elevator,0))
-        #6/1
-
         self.AuxController.a().onTrue(cannonToPosition(self.cannon, 0.108))
         self.AuxController.b().onTrue(cannonToPosition(self.cannon, 0.133))
         self.AuxController.y().onTrue(cannonToPosition(self.cannon, 0.))
         self.AuxController.x().onTrue(cannonToPosition(self.cannon, 0.297))
-
-        self.AuxController.axisGreaterThan(1,.5).whileTrue(elevatorDown(self.elevator))
-        self.AuxController.axisLessThan(1,-.5).whileTrue(elevatorUp(self.elevator))
         
-        self.AuxController.axisLessThan(5,-.5).whileTrue(PivotUp(self.cannon))
-        self.AuxController.axisGreaterThan(5,.5).whileTrue(PivotDown(self.cannon))
+        self.AuxController.axisGreaterThan(1, 0.5).whileTrue(elevatorDown(self.elevator))
+        self.AuxController.axisLessThan(1, -0.5).whileTrue(elevatorUp(self.elevator))
+        self.AuxController.axisLessThan(5, -0.5).whileTrue(PivotUp(self.cannon))
+        self.AuxController.axisGreaterThan(5, 0.5).whileTrue(PivotDown(self.cannon))
         
-
-
         self.joystickButton4 = self.joystick.button(4)
         self.joystickButton4.onTrue(SlowDown(self.driveTrain))
         
-        #Welcome to the Ryan Zone 
+        # Bind the Lock command with both limelight and driveTrain.
         self.joystickButton2 = self.joystick.button(2)
-        self.joystickButton2.whileTrue(Lock(self.limelight))
-
+        self.joystickButton2.whileTrue(Lock(self.limelight, self.driveTrain))
         
-        #self.AuxController.start().onTrue(elevatorToPos(self.elevator,0.5))
-        self.AuxController.rightStick().onTrue(elevatorToPos(self.elevator,0.0))
-        self.AuxController.leftStick().onTrue(cannonToPosition(self.cannon,0.5))
-        #self.AuxController.leftStick().whileTrue(elevatorUp(self.elevator))
-
-
+        self.AuxController.rightStick().onTrue(elevatorToPos(self.elevator, 0.0))
+        self.AuxController.leftStick().onTrue(cannonToPosition(self.cannon, 0.5))
         self.AuxController.pov(0).whileTrue(GrabAlgae(self.algae))
         self.AuxController.pov(180).whileTrue(ReleaseAlgae(self.algae))
 
     def getAutonomousCommand(self) -> commands2.Command:
-        """Use this to pass the autonomous command to the main {@link Robot} class.
-
-        :returns: the command to run in autonomous
-        """
-        
-        path = self._auto_chooser.getSelected()
-        return path
-      
+        return self._auto_chooser.getSelected()
 
     def checkJoystickInput(self, kInput: float):
-        #getting values out of the joystick to do things
         if kInput < 0.1:
             kInput = 0
-            print ("input = 0")
+            print("input = 0")
         else:
-            kInput = kInput/2
-            print("input = " + kInput)
+            kInput = kInput / 2
+            print("input = " + str(kInput))
         return kInput
     
     def disabledInit(self):
-        elevatorToPos(self.elevator,0)
+        elevatorToPos(self.elevator, 0)
 
     def autonomousInit(self):
         pass
@@ -148,7 +110,6 @@ class RobotContainer():
         pass
 
     def teleopInit(self):
-        #self.driveTrain.setSwerveStates(1.0, 1.0, 1.0)
         pass
 
     def teleopPeriodic(self):
--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -5,164 +5,182 @@ from subsystems.swerveModule import SwerveModule
 from wpilib import DriverStation
 from wpimath import controller, trajectory, estimator
 import wpimath
-from math import hypot, radians, pi, atan2
-from typing import List
+from math import hypot, pi
+from typing import Tuple
 import commands2
 import RobotConfig as rc
 import math
+
 class DriveTrainSubSystem(commands2.Subsystem):
     def __init__(self, joystick: commands2.button.CommandJoystick) -> None:
-        #camera settings
-        self.stateStdDevs = 0.0, 0.0, 0.0
-        self.visionMeasurementsStdDevs = 1.0, 1.0, 1.0
+        # Camera settings
+        self.stateStdDevs = (0.0, 0.0, 0.0)
+        self.visionMeasurementsStdDevs = (1.0, 1.0, 1.0)
 
-        #set up the joystick and navx sensor
+        # Set up the joystick and navx sensor
         self.joystick = joystick
         self.navx = navx.AHRS.create_spi()
 
-        #getting some important constants about the robot declared
+        # Getting important constants from RobotConfig
         self.kMaxSpeed = rc.driveConstants.RobotSpeeds.maxSpeed
-        self.kMaxAngularVelocity = rc.driveConstants.RobotSpeeds.maxSpeed /hypot(rc.robotDimensions.trackWidth / 2, rc.robotDimensions.wheelBase / 2)
+        self.kMaxAngularVelocity = rc.driveConstants.RobotSpeeds.maxSpeed / hypot(rc.robotDimensions.trackWidth / 2, rc.robotDimensions.wheelBase / 2)
         self.wheelBase = rc.robotDimensions.wheelBase
         self.trackWidth = rc.robotDimensions.trackWidth
         self.speedMultiplier = 1
 
-        #defining the location of each swerve module on the can chain
-        self.frontLeft = SwerveModule(rc.SwerveModules.frontLeft.driveMotorID, rc.SwerveModules.frontLeft.turnMotorID, rc.SwerveModules.frontLeft.CANCoderID, rc.SwerveModules.frontLeft.encoderOffset, rc.SwerveModules.frontLeft.isInverted)
-        self.frontRight = SwerveModule(rc.SwerveModules.frontRight.driveMotorID, rc.SwerveModules.frontRight.turnMotorID, rc.SwerveModules.frontRight.CANCoderID, rc.SwerveModules.frontRight.encoderOffset, rc.SwerveModules.frontRight.isInverted)
-        self.rearLeft = SwerveModule(rc.SwerveModules.rearLeft.driveMotorID, rc.SwerveModules.rearLeft.turnMotorID, rc.SwerveModules.rearLeft.CANCoderID, rc.SwerveModules.rearLeft.encoderOffset, rc.SwerveModules.rearLeft.isInverted)
-        self.rearRight = SwerveModule(rc.SwerveModules.rearRight.driveMotorID, rc.SwerveModules.rearRight.turnMotorID, rc.SwerveModules.rearRight.CANCoderID, rc.SwerveModules.rearRight.encoderOffset, rc.SwerveModules.rearRight.isInverted)
+        # Define the location of each swerve module
+        self.frontLeft = SwerveModule(rc.SwerveModules.frontLeft.driveMotorID,
+                                      rc.SwerveModules.frontLeft.turnMotorID,
+                                      rc.SwerveModules.frontLeft.CANCoderID,
+                                      rc.SwerveModules.frontLeft.encoderOffset,
+                                      rc.SwerveModules.frontLeft.isInverted)
+        self.frontRight = SwerveModule(rc.SwerveModules.frontRight.driveMotorID,
+                                       rc.SwerveModules.frontRight.turnMotorID,
+                                       rc.SwerveModules.frontRight.CANCoderID,
+                                       rc.SwerveModules.frontRight.encoderOffset,
+                                       rc.SwerveModules.frontRight.isInverted)
+        self.rearLeft = SwerveModule(rc.SwerveModules.rearLeft.driveMotorID,
+                                     rc.SwerveModules.rearLeft.turnMotorID,
+                                     rc.SwerveModules.rearLeft.CANCoderID,
+                                     rc.SwerveModules.rearLeft.encoderOffset,
+                                     rc.SwerveModules.rearLeft.isInverted)
+        self.rearRight = SwerveModule(rc.SwerveModules.rearRight.driveMotorID,
+                                      rc.SwerveModules.rearRight.turnMotorID,
+                                      rc.SwerveModules.rearRight.CANCoderID,
+                                      rc.SwerveModules.rearRight.encoderOffset,
+                                      rc.SwerveModules.rearRight.isInverted)
         
-        #renaming some variables so they are easier to use
+        # Renaming some variables so they are easier to use
         teleopConstants = rc.driveConstants.poseConstants
         
-        #setting up how we send info to the wheels about the position of the turn motor, this is mostly for if we want a theta override function for auto place
+        # Setup PID for rotation override
         rotationConstants = rc.driveConstants.ThetaPIDConstants.translationPIDConstants
         self.rotationPID = controller.PIDController(rotationConstants.kP, rotationConstants.kI, rotationConstants.kD, rotationConstants.period)
         self.rotationPID.enableContinuousInput(-pi, pi)
         
-        #a pose helps the robot know where it is, the alliance thing is for autos
-        self.poseTolerance = geometry.Pose2d(geometry.Translation2d(x=teleopConstants.xPoseToleranceMeters, y=teleopConstants.yPoseToleranceMeters), geometry.Rotation2d(teleopConstants.thetaPoseToleranceRadians))
+        # Pose estimation and tolerance
+        self.poseTolerance = geometry.Pose2d(
+            geometry.Translation2d(x=teleopConstants.xPoseToleranceMeters, y=teleopConstants.yPoseToleranceMeters),
+            geometry.Rotation2d(teleopConstants.thetaPoseToleranceRadians))
         self.alliance = wpilib.DriverStation.Alliance.kBlue
         
-        #classes to help proccess info about where the robot is and how fast and in what direction it is moving
-        self.KINEMATICS = kinematics.SwerveDrive4Kinematics(geometry.Translation2d(float(self.trackWidth / 2), float(self.wheelBase / 2)), geometry.Translation2d(float(self.trackWidth / 2), float(-self.wheelBase / 2)), geometry.Translation2d(float(-self.trackWidth / 2), float(self.wheelBase / 2)), geometry.Translation2d(float(-self.trackWidth / 2), float(-self.wheelBase / 2)))
-        #self.poseEstimatior = estimator.SwerveDrive4PoseEstimator(kinematics.SwerveDrive4Kinematics(geometry.Translation2d(float(self.trackWidth / 2), float(self.wheelBase / 2)), geometry.Translation2d(float(self.trackWidth / 2), float(-self.wheelBase / 2)), geometry.Translation2d(float(-self.trackWidth / 2), float(self.wheelBase / 2)), geometry.Translation2d(float(-self.trackWidth / 2), float(-self.wheelBase / 2))), 
-                                                                  #self.getNavxRotation2d(), self.getSwerveModulePositions(), geometry.Pose2d(0, 0, geometry.Rotation2d()), self.stateStdDevs, self.visionMeasurementsStdDevs)
-        self.poseEstimatior = estimator.SwerveDrive4PoseEstimator(self.KINEMATICS, self.getNavxRotation2d(), self.getSwerveModulePositions(), geometry.Pose2d(geometry.Translation2d(), geometry.Rotation2d()), self.stateStdDevs, self.visionMeasurementsStdDevs)
+        # Kinematics and Pose Estimator
+        self.KINEMATICS = kinematics.SwerveDrive4Kinematics(
+            geometry.Translation2d(float(self.trackWidth / 2), float(self.wheelBase / 2)),
+            geometry.Translation2d(float(self.trackWidth / 2), float(-self.wheelBase / 2)),
+            geometry.Translation2d(float(-self.trackWidth / 2), float(self.wheelBase / 2)),
+            geometry.Translation2d(float(-self.trackWidth / 2), float(-self.wheelBase / 2)))
+        
+        self.poseEstimatior = estimator.SwerveDrive4PoseEstimator(
+            self.KINEMATICS,
+            self.getNavxRotation2d(),
+            self.getSwerveModulePositions(),
+            geometry.Pose2d(geometry.Translation2d(), geometry.Rotation2d()),
+            self.stateStdDevs,
+            self.visionMeasurementsStdDevs)
 
         self.field = wpilib.Field2d()
         wpilib.SmartDashboard.putData("Field: ", self.field)
-        
-    def getNavxRotation2d(self)-> geometry.Rotation2d:
-        #getting the direction the robot is facing relative to where we started for field orient
-        return geometry.Rotation2d(math.tau - self.navx.getRotation2d().radians()) #this inverts the reading from the navx which lets the robot work with the inverted cancoders. likely not neccesary next year.
-    
-    def getPose(self)-> geometry.Pose2d:
-        #figuring out where the robot is relative to where we started
+
+        # Joystick override variable
+        self.joystickOverride: Tuple[float, float, float] = None
+
+    def getNavxRotation2d(self) -> geometry.Rotation2d:
+        # Get the robot's heading (inverted to account for sensor mounting)
+        return geometry.Rotation2d(math.tau - self.navx.getRotation2d().radians())
+
+    def getPose(self) -> geometry.Pose2d:
+        # Return the current estimated pose
         return self.poseEstimatior.getEstimatedPosition()
-    
+
     def getSwerveModulePositions(self):
-        #figuring out where the wheels are relative to where they started
-        return self.frontLeft.getPosition(), self.frontRight.getPosition(), self.rearLeft.getPosition(), self.rearRight.getPosition()
+        # Return the positions of each swerve module
+        return (self.frontLeft.getPosition(), self.frontRight.getPosition(),
+                self.rearLeft.getPosition(), self.rearRight.getPosition())
     
-    def resetPose(self, poseToSet: geometry.Pose2d)-> None:
-        #we broke our pose so we are resetting it with our current location as 0
+    def resetPose(self, poseToSet: geometry.Pose2d) -> None:
+        # Reset the pose estimator with a new pose
         self.poseEstimatior.resetPosition(self.getNavxRotation2d(), self.getSwerveModulePositions(), poseToSet)
     
-    def resetFieldOrient(self)-> None:
-        #this doesnt do anything useful
+    def resetFieldOrient(self) -> None:
         self.navx.reset()
     
-    def getJoystickInput(self)-> tuple[float]:
-        #getting input from the joysticks and changing it so that we can use it
+    def getJoystickInput(self) -> Tuple[float, float, float]:
+        # If an override is set, return it; otherwise, process real joystick input.
+        if self.joystickOverride is not None:
+            return self.joystickOverride
         constants = rc.driveConstants.joystickConstants
-        '''print('getting input.')
-        print('x value: ' + str(self.joystick.getX()))
-        print('y value: ' + str(self.joystick.getY()))
-        print('z value: ' + str(self.joystick.getZ()))'''
-        #print("joystick y: " + str(-wpimath.applyDeadband(self.joystick.getY(), constants.yDeadband)))
-        
         deadbandedY = -wpimath.applyDeadband(self.joystick.getY(), constants.yDeadband)
-        deadbandedX = wpimath.applyDeadband(self.joystick.getX(), constants.xDeadband) #need to invert the x direction this accounts for inverted cancoders
+        deadbandedX = wpimath.applyDeadband(self.joystick.getX(), constants.xDeadband)
         deadbandedZ = -wpimath.applyDeadband(self.joystick.getZ(), constants.theataDeadband)
-        
-
-        return(deadbandedY, deadbandedX, deadbandedZ)
+        return (deadbandedY, deadbandedX, deadbandedZ)
     
-    def setSwerveStates(self, xSpeed: float, ySpeed: float, zSpeed: float, fieldOrient = True)-> None:
-        #using the input from the get joystick input function to tell the wheels where to go
+    def setSwerveStates(self, xSpeed: float, ySpeed: float, zSpeed: float, fieldOrient: bool = True) -> None:
         if fieldOrient:
-            SwerveModuleStates = self.KINEMATICS.toSwerveModuleStates(kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, zSpeed, self.poseEstimatior.getEstimatedPosition().rotation()))
-
+            SwerveModuleStates = self.KINEMATICS.toSwerveModuleStates(
+                kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(
+                    xSpeed, ySpeed, zSpeed, self.poseEstimatior.getEstimatedPosition().rotation()))
         else:
-            SwerveModuleStates = self.KINEMATICS.toSwerveModuleStates(kinematics.ChassisSpeeds(xSpeed, ySpeed, zSpeed))
+            SwerveModuleStates = self.KINEMATICS.toSwerveModuleStates(
+                kinematics.ChassisSpeeds(xSpeed, ySpeed, zSpeed))
         self.frontLeft.setState(SwerveModuleStates[0])
         self.frontRight.setState(SwerveModuleStates[1])
         self.rearLeft.setState(SwerveModuleStates[2])
         self.rearRight.setState(SwerveModuleStates[3])
         wpilib.SmartDashboard.putNumber("current rotation", self.poseEstimatior.getEstimatedPosition().rotation().degrees())
-
-
         wpilib.SmartDashboard.putBoolean("have navx: ", self.navx.isConnected())
         wpilib.SmartDashboard.putNumber("last rotation: ", self.getCurrentPose().rotation().degrees())
         wpilib.SmartDashboard.putNumber("x distance: ", self.getCurrentPose().translation().X())
         wpilib.SmartDashboard.putNumber("y distance: ", self.getCurrentPose().translation().Y())
         wpilib.SmartDashboard.putNumber("navx position: ", self.getNavxRotation2d().degrees())
 
-
-    
-    def joystickDrive(self, inputs: tuple[float])-> None:
-        #proccessing the joystick input values and sending them to the set swerve states function
-        xSpeed, ySpeed, zSpeed, = (inputs[0] * self.kMaxSpeed,
-                                   inputs[1] * self.kMaxSpeed,
-                                   inputs[2] * self.kMaxAngularVelocity * rc.driveConstants.RobotSpeeds.manualRotationSpeedFactor)
-        #print(self.navx.getAngle())
+    def joystickDrive(self, inputs: Tuple[float, float, float]) -> None:
+        # Process joystick inputs and drive the robot accordingly.
+        xSpeed = inputs[0] * self.kMaxSpeed
+        ySpeed = inputs[1] * self.kMaxSpeed
+        zSpeed = inputs[2] * self.kMaxAngularVelocity * rc.driveConstants.RobotSpeeds.manualRotationSpeedFactor
         self.setSwerveStates(xSpeed, ySpeed, zSpeed, True)
-        print("k",self.kMaxSpeed)
+        print("k", self.kMaxSpeed)
 
-    def autoDrive(self, chasssisSpeeds: kinematics.ChassisSpeeds, currentPose: geometry.Pose2d, fieldRelative = True):
-        if chasssisSpeeds == kinematics.ChassisSpeeds(0, 0, 0):
+    def autoDrive(self, chassisSpeeds: kinematics.ChassisSpeeds, currentPose: geometry.Pose2d, fieldRelative: bool = True):
+        if chassisSpeeds == kinematics.ChassisSpeeds(0, 0, 0):
             self.stationary()
         else:
-            chasssisSpeeds.omega = -chasssisSpeeds.omega
+            chassisSpeeds.omega = -chassisSpeeds.omega
             if fieldRelative:
-                swerveModuleStates = self.KINEMATICS.toSwerveModuleStates(kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(chasssisSpeeds.vx, chasssisSpeeds.vy, chasssisSpeeds.omega, currentPose.rotation()))
+                swerveModuleStates = self.KINEMATICS.toSwerveModuleStates(
+                    kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(chassisSpeeds.vx, chassisSpeeds.vy, chassisSpeeds.omega, currentPose.rotation()))
             else:
-                swerveModuleStates = self.KINEMATICS.toSwerveModuleStates(chasssisSpeeds)
-            
+                swerveModuleStates = self.KINEMATICS.toSwerveModuleStates(chassisSpeeds)
             self.frontLeft.setState(swerveModuleStates[0])
             self.frontRight.setState(swerveModuleStates[1])
             self.rearLeft.setState(swerveModuleStates[2])
             self.rearRight.setState(swerveModuleStates[3])
-            
     
-    def stationary(self)-> None:
-        #stop the robot by breaking all the motors
+    def stationary(self) -> None:
+        # Stop all drive motors.
         self.frontLeft.stop()
         self.frontRight.stop()
         self.rearLeft.stop()
         self.rearRight.stop()
     
-    def coast(self)-> None:
-        # coast the robot
+    def coast(self) -> None:
         self.frontLeft.setNuetral()
         self.frontRight.setNuetral()
         self.rearLeft.setNuetral()
         self.rearRight.setNuetral()
     
     def getRobotRelativeChassisSpeeds(self):
-        #where are all the wheels relative to where they started, not really sure what this is for
-        states = (self.frontLeft.getPosition(), self.frontRight.getPosition(), self.rearLeft.getPosition(), self.rearRight.getPosition())
+        states = (self.frontLeft.getPosition(), self.frontRight.getPosition(),
+                  self.rearLeft.getPosition(), self.rearRight.getPosition())
         return self.KINEMATICS.toChassisSpeeds(states)
     
-    def getCurrentPose(self)-> geometry.Pose2d:
-        # updating the current position of the robot
+    def getCurrentPose(self) -> geometry.Pose2d:
         return self.poseEstimatior.getEstimatedPosition()
     
     def halfSpeed(self):
-        #self.kMaxSpeed = 0.5 * (rc.driveConstants.RobotSpeeds.maxSpeed)
         pass
+
     def fullSpeed(self):
         self.kMaxSpeed = rc.driveConstants.RobotSpeeds.maxSpeed
     
